0687a367460fcb7420b9aabd2abec10e5a51ad9d
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2022-12-18 22:04:56 -0600

    Gulp libraries install

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
-const { src, dest } = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
+// Compilar SASS a CSS
 function css(done) {
   // Identificar archivo SASS
-  src("src/scss/app.scss")
+  src('src/scss/**/*.scss')
+    // Librería para evitar la detención del watch en un error
+    .pipe(plumber())
     // Compilar
     .pipe(sass())
     // Almacenar
-    .pipe(dest("build/css"));
+    .pipe(dest('build/css'));
 
   done(); // Callback que avisa a Gulp que hemos terminado
 }
 
+// Monitor de cambios de CSS
+function dev(done) {
+  watch('src/scss/**/*.scss', css);
+
+  done();
+}
+
 exports.css = css;
+exports.dev = dev;
