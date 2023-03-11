861d76b53d5e0364e191d005bbde09a48b7a8f19
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-03-10 18:37:51 -0600

    Structure Website Complete

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,41 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function startApp() {
+  fixedNav();
   createGallery();
+  scrollNav();
+}
+
+function fixedNav() {
+  const bar = document.querySelector('.header');
+  const scrolling = document.querySelector('.about-festival');
+  const body = document.querySelector('body');
+
+  window.addEventListener('scroll', function () {
+    if (scrolling.getBoundingClientRect().top < 0) {
+      bar.classList.add('fixed-scrollbar');
+      body.classList.add('fixed-body');
+    } else {
+      bar.classList.remove('fixed-scrollbar');
+      body.classList.remove('fixed-body');
+    }
+  });
+
+}
+
+function scrollNav() {
+  const links = document.querySelectorAll('.main-navigation a');
+
+  links.forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const sectionScroll = e.target.attributes.href.value;
+      const section = document.querySelector(sectionScroll);
+
+      section.scrollIntoView({behavior: 'smooth'});
+    });
+  });
 }
 
 function createGallery() {
@@ -28,6 +62,7 @@ function createGallery() {
 
 function showImage(id) {
   const image = document.createElement('picture');
+
   image.innerHTML =
     `<source srcset="build/img/grande/${id}.avif" type="image/avif">
      <source srcset="build/img/grande/${id}.webp" type="image/webp">
@@ -35,20 +70,24 @@ function showImage(id) {
 
   // Create Image Overlay
   const overlay = document.createElement('DIV');
+
   overlay.appendChild(image);
   overlay.classList.add('gallery-overlay');
   overlay.onclick = function () {
     const body = document.querySelector('body');
+
     body.classList.remove('body-hold');
     overlay.remove();
   }
 
   // Kill Overlay
   const closeImage = document.createElement('P');
+
   closeImage.textContent = 'X';
   closeImage.classList.add('btn-close');
   closeImage.onclick = function () {
     const body = document.querySelector('body');
+
     body.classList.remove('body-hold');
     overlay.remove();
   };
@@ -56,6 +95,7 @@ function showImage(id) {
 
   // Attach Overlay over HTML
   const body = document.querySelector('body');
+
   body.appendChild(overlay);
   body.classList.add('body-hold');
 }
